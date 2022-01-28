5d37aba9ec338ef178b497882a747bd06a8601d3
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-01-28 17:02:06 -0500

    fixing suitesparse build

--- scripts/suitesparse_default_build.sh
+++ scripts/suitesparse_default_build.sh
@@ -9,26 +9,36 @@ _usage() {
 SRCDIR=${TwoPiRoot}/src
 REPO=${SRCDIR}/${SUITESPARSE_REPO}
 
+cd ${REPO}/AMD
+make 
+make install INSTALL=${TwoPiRoot}
+
+cd ${REPO}/CAMD
+make 
+make install INSTALL=${TwoPiRoot}
+
+cd ${REPO}/CCOLAMD
+make 
+make install INSTALL=${TwoPiRoot}
+
+cd ${REPO}/CHOLMOD
+make 
+make install INSTALL=${TwoPiRoot}
+
+cd ${REPO}/COLAMD
+make 
+make install INSTALL=${TwoPiRoot}
+
+cd ${REPO}/BTF
+make 
+make install INSTALL=${TwoPiRoot}
+
 cd ${REPO}/KLU
 make  MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
 
 cd ${REPO}/UMFPACK
 make
 
-cd ${REPO}/AMD
-make install INSTALL=${TwoPiRoot}
-
-cd ${REPO}/CAMD
-make install INSTALL=${TwoPiRoot}
-
-cd ${REPO}/CCOLAMD
-make install INSTALL=${TwoPiRoot}
-
-cd ${REPO}/CHOLMOD
-make install INSTALL=${TwoPiRoot}
-
-cd ${REPO}/COLAMD
-make install INSTALL=${TwoPiRoot}
 
 cd ${REPO}/KLU
 make install INSTALL=${TwoPiRoot}
