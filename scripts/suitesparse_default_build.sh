c1911790275587ba2d325cb54e219e0329c1407b
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-03-04 09:13:51 -0500

    spcify compilers

--- scripts/suitesparse_default_build.sh
+++ scripts/suitesparse_default_build.sh
@@ -10,42 +10,59 @@ SRCDIR=${TwoPiRoot}/src
 REPO=${SRCDIR}/${SUITESPARSE_REPO}
 
 cd ${REPO}/SuiteSparse_config
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/AMD
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/CAMD
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/CCOLAMD
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/COLAMD
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/CHOLMOD
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/BTF
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-make install INSTALL=${TwoPiRoot}
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/KLU
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/UMFPACK
-make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include
-
+make MY_METIS_LIB=${TwoPiRoot}/lib/libmetis.so MY_METIS_INC=${TwoPiRoot}/include \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/KLU
-make install INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
 
 cd ${REPO}/UMFPACK
-make install INSTALL=${TwoPiRoot}
+make install INSTALL=${TwoPiRoot} \
+     F77=${FC} CC=${CC} CXX=${CXX} INSTALL=${TwoPiRoot}
