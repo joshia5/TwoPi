810a302a3d4ac0421de1a5de13ba42e1dc5d98fe
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-03-15 13:13:38 -0400

    added hypre cuda option

--- scripts/hypre_default_build.sh
+++ scripts/hypre_default_build.sh
@@ -1,12 +1,39 @@
 #!/bin/bash
 
+SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
+source $SCRIPT
+
+_usage() {
+    echo 'HYPRE : parallel linear solver'
+    echo '   options: --cuda (nvcc must be found on PATH)'
+}
+
+ENABLE_CUDA="NO"
+
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --cuda)
+    ENABLE_CUDA="YES"	
+    shift # past argument    	
+    ;;
+    --help)
+    _usage
+    exit 1
+    ;;
+    *)
+    echo "Unknown option " $key
+    exit 2  #  error_code=2
+    ;;
+esac
+done
+
 GIT=$(command -v git)
 SRCDIR=${TwoPiRoot}/src
 CMAKE=$(command -v cmake)
 MAKE=$(command -v make)
 
-SCRIPT=$(dirname "$0")/env_${TwoPiDevice}.sh
-source $SCRIPT
 
 HYPREDIR=${SRCDIR}/hypre-${HYPRE_VERSION}
 
@@ -20,6 +47,7 @@ $CMAKE .. -DCMAKE_VERBOSE_MAKEFILE=1                                     \
           -DBUILD_SHARED_LIBS=1                                          \
           -DHYPRE_INSTALL_PREFIX=${TwoPiRoot}                            \
           -DHYPRE_SHARED=1                                               \
+          -DHYPRE_WITH_CUDA=${ENABLE_CUDA}                               \
           -DCMAKE_INSTALL_PREFIX=${TwoPiRoot}                            \
           -DCMAKE_INSTALL_NAME_DIR=${TwoPiRoot}/lib                            
 
