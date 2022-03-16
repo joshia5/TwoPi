6578c510e13ca2bb6807e54298c566b576632dfd
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-03-16 13:38:26 -0400

    added stellar_intel

---
+++ scripts/env_stellar_intel.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+CC=icc
+CXX=icpc
+FC=ifort
+FL=ifort
+MPICC=mpiicc
+MPICXX=mpiicpc
+MPIFC=mpiiffort
+MPIFL=mpiifort
+OMPFLAG="-qopenmp"
+OMPLINKFLAG=-qopenmp
+OMPCXXFLAG=-qopenmp
+OMPCCFLAG=-qopenmp
+OMPFCFLAG=-qopenmp
+
+source $(dirname $BASH_SOURCE)/env_common.sh
+
+# path to mpi.h
+#    (note) It appears find_mpi does not work with CC (cray MPI compiler)
+# MPI_INCLUDE_PATH=/opt/cray/pe/mpt/7.7.10/gni/mpich-intel/16.0/include
+# MPI_LIBRARY_PATH=/opt/cray/pe/mpt/7.7.10/gni/mpich-intel/16.0/lib
+
+# enforce scotch to 6.0.6
+#SCOTCH_DL=37622
+#SCOTCH_VERSION=6.0.6
+
+# CUDA_ARCHITECUTRES (will be passed by DCMAKE_CUDA_ARCHITECTURES)
+CUDA_ARCH=80
+
