8b07bf40a48a42c18f28631b66bb4d5584c6863c
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-10-11 16:52:29 -0400

    changed Metis/ParMeits location

--- scripts/metis_default_clone.sh
+++ scripts/metis_default_clone.sh
@@ -7,14 +7,15 @@ REPO="METIS"
 mkdir -p $SRCDIR
 cd $SRCDIR
 
+wget https://github.com/mfem/tpls/raw/gh-pages/metis-5.1.0.tar.gz
 #wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/metis/metis-5.1.0.tar.gz
-#tar -zxvf metis-5.1.0.tar.gz
-#rm metis-5.1.0.tar.gz
+tar -zxvf metis-5.1.0.tar.gz
+rm metis-5.1.0.tar.gz
 
-SC=$(dirname "$0")/subs/git_access.sh
-source $SC
+#SC=$(dirname "$0")/subs/git_access.sh
+#source $SC
 
-git_clone_or_pull "https://github.com/KarypisLab/METIS.git"  $REPO $SRCDIR
+#git_clone_or_pull "https://github.com/KarypisLab/METIS.git"  $REPO $SRCDIR
 
 
 
