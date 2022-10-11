bbad6d09f15bc07a0b7a773af3d56a3f710a8b3f
Syun'ichi Shiraiwa <shiraiwa@princeton.edu>
2022-10-11 16:55:43 -0400

    changed Metis/ParMeits location

--- scripts/parmetis_default_clone.sh
+++ scripts/parmetis_default_clone.sh
@@ -11,6 +11,7 @@ wget https://github.com/sshiraiwa/tpls/raw/gh-pages/parmetis-4.0.3.tar.gz
 #wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
 tar -zxvf parmetis-4.0.3.tar.gz
 rm parmetis-4.0.3.tar.gz
+mv parmetis-4.0.3 ParMETIS
 
 #SC=$(dirname "$0")/subs/git_access.sh
 #source $SC
