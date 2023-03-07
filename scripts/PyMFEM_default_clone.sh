7dc9871f7a50257b57ad0ecccba32d5dfd60f622
joshia5 <joshia5@rpi.edu>
2023-03-07 12:54:02 -0500

    use ssh

--- scripts/PyMFEM_default_clone.sh
+++ scripts/PyMFEM_default_clone.sh
@@ -33,7 +33,7 @@ done
 SC=$(dirname "$0")/subs/git_access.sh
 source $SC
 
-git_clone_or_pull "https://github.com/joshia5/PyMFEM.git" $REPO $SRCDIR
+git_clone_or_pull "git@github.com:joshia5/PyMFEM.git" $REPO $SRCDIR
 
 if [ ! -f ${SRCDIR}/${REPO} ]; then
    cd ${SRCDIR}/${REPO}
