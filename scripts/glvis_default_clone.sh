9ad572c38c7519e5423344e4280029981e644c93
joshia5 <joshia5@rpi.edu>
2023-03-08 23:17:19 -0500

    update glvis clone location

--- scripts/glvis_default_clone.sh
+++ scripts/glvis_default_clone.sh
@@ -7,8 +7,4 @@ SRCDIR=${TwoPiRoot}/src
 SC=$(dirname "$0")/subs/git_access.sh
 source $SC
 
-git_clone_or_pull "https://github.com/glvis/glvis.git" $REPO $SRCDIR
-
-
-
-
+git_clone_or_pull "git@github.com:joshia5/glvis.git" $REPO $SRCDIR
